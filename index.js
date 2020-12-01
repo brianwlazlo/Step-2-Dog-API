41f12d1bef8e1d57da373457c1d6ba11eccafa21
brianwlazlo <73142386+brianwlazlo@users.noreply.github.com>
2020-12-01 00:06:07 -0500

    Add files via upload
    upload html, js and css files to main

---
+++ index.js
@@ -0,0 +1,44 @@
+//get dog images
+function getDogImages(numOfDogs) {
+    if (numOfDogs < 3 || numOfDogs > 50) {
+        $('#number-of-dogs').val('');
+        return alert("Please only enter a number between 3 and 50");
+    } else {
+        fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
+            .then(response=> response.json())
+            .then(responseJson => generatePicsHtml(responseJson))
+            .catch(error => alert("Somthing went wrong"));
+    }
+    $('#number-of-dogs').val('');
+}
+
+// generate html to add to DOM
+function generatePicsHtml (responseJson) {
+    console.log(responseJson);
+    let resultsArray = responseJson.message;
+    let resultsHtml = '';
+
+    resultsArray.forEach(url => {
+        resultsHtml += `
+            <div id="result-container">
+                <img src="${url}" class='results-img'>
+            </div>`
+    })
+
+    $('#result-pictures').html(resultsHtml)
+}
+
+// watch for user input
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      let numOfDogs = $('#number-of-dogs').val();
+      getDogImages( numOfDogs );
+    });
+}
+
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+});
